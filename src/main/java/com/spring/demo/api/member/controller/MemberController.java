554d9644372105563e4b467c73dc9fa726800496
김수찬 <tncks097@naver.com>
2023-08-15 19:33:57 +0900

    Feat : 스웨거 등록

--- src/main/java/com/spring/demo/api/member/controller/MemberController.java
+++ src/main/java/com/spring/demo/api/member/controller/MemberController.java
@@ -6,8 +6,9 @@ import com.spring.demo.domain.member.dto.MemberInformDTO;
 import com.spring.demo.domain.member.dto.MemberJoinDTO;
 import com.spring.demo.domain.member.dto.MemberLoginDTO;
 import com.spring.demo.domain.member.entity.Member;
-import com.spring.demo.domain.member.exception.ApiException;
 import com.spring.demo.global.jwt.dto.TokenDTO;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
@@ -17,20 +18,23 @@ import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequestMapping("/api/member")
+@Tag(name = "Member 컨트롤러", description = "Member Controller API Document")
 public class MemberController {
-    private final MemberService memberService;
 
+    private final MemberService memberService;
     public MemberController(MemberService memberService){
         this.memberService = memberService;
     }
 
     @PostMapping("/signup")
+    @Operation(summary = "회원가입", description = "회원가입을 진행합니다", tags = {"View"})
     public ResponseEntity<? extends BaseResponseDTO<String>> signUp(@RequestBody MemberJoinDTO memberInfo) {
         BaseResponseDTO<String> response = memberService.signUp(memberInfo);
         return ResponseEntity.status(response.getStatusCode()).body(response);
     }
 
     @PostMapping("/signin")
+    @Operation(summary = "로그인", description = "Id, PW로 로그인 진행", tags = {"View"})
     public ResponseEntity<? extends BaseResponseDTO<TokenDTO>> signIn(@RequestBody MemberLoginDTO memberInfo, HttpServletResponse httpResponse){
         BaseResponseDTO<TokenDTO> response = memberService.signIn(memberInfo, httpResponse);
         return ResponseEntity.status(response.getStatusCode()).body(response);
@@ -38,16 +42,11 @@ public class MemberController {
     }
 
     @PostMapping("/inform")
+    @Operation(summary = "사용자 정보", description = "Post를 통해 받은 Email을 통해 정보를 받아옴", tags = {"View"})
     public  ResponseEntity<? extends BaseResponseDTO<Member>> getInfo(@RequestBody MemberInformDTO memberInfo){
 //        System.out.println("제블 드러와줘");
         BaseResponseDTO<Member> response = memberService.getInfo(memberInfo);
         return ResponseEntity.status(response.getStatusCode()).body(response);
     }
 
-    @PostMapping("/test")
-    public ResponseEntity<? extends BaseResponseDTO<Member>> test(@RequestBody MemberLoginDTO memberInfo){
-//        System.out.println("제블 드러와줘");
-        BaseResponseDTO<String> response = memberService.test(memberInfo);
-        return null;
-    }
 }
