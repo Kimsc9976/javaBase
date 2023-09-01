4f53c45076875ad109a8e15f2d8c4ca07cf9dcd4
김수찬 <tncks097@naver.com>
2023-09-02 02:23:07 +0900

    Feat: 스웨거 테그 설정

--- src/main/java/com/spring/demo/api/member/controller/MemberController.java
+++ src/main/java/com/spring/demo/api/member/controller/MemberController.java
@@ -28,49 +28,49 @@ public class MemberController {
     private final EmailService emailService;
 
     @PostMapping("/signup")
-    @Operation(summary = "회원가입", description = "회원가입을 진행합니다", tags = {"View"})
+    @Operation(summary = "회원가입", description = "회원가입을 진행합니다", tags = {"회원 가입"})
     public ResponseEntity<? extends BaseResponseDTO<String>> signUp(@RequestBody MemberJoinDTO memberInfo) {
         BaseResponseDTO<String> response = memberService.signUp(memberInfo);
         return ResponseEntity.status(response.getStatusCode()).body(response);
     }
 
     @PostMapping("/verifyMember")
-    @Operation(summary = "이메일 중복확인", description = "이메일이 중복이 되는지 확인합니다.", tags = {"View"})
+    @Operation(summary = "이메일 중복확인", description = "이메일이 중복이 되는지 확인합니다.", tags = {"회원 가입"})
     public ResponseEntity<? extends BaseResponseDTO<String>> verifyMember(@RequestBody MemberInformDTO memberInfo){
         BaseResponseDTO<String> response = memberService.verifyMember(memberInfo);
         return ResponseEntity.status(response.getStatusCode()).body(response);
     }
 
     @GetMapping("/verifyAuthNum")
-    @Operation(summary = "이메일 인증[이메일 전송]", description = "이메일이 중복이 되는지 확인을 위한 메일 전송을 진행합니다..", tags = {"View"})
+    @Operation(summary = "이메일 인증[이메일 전송]", description = "이메일이 중복이 되는지 확인을 위한 메일 전송을 진행합니다..", tags = {"회원 가입"})
     public ResponseEntity<? extends BaseResponseDTO<String>> sendMail(@RequestParam String email, @RequestParam String type)
             throws MessagingException {
         BaseResponseDTO<String> response = emailService.sendMail(email, type);
         return ResponseEntity.status(response.getStatusCode()).body(response);
     }
     @PostMapping("/verifyAuthNum")
-    @Operation(summary = "이메일 인증[이메일 확인]", description = "이메일이 중복이 되는지 확인합니다.", tags = {"View"})
+    @Operation(summary = "이메일 인증[이메일 확인]", description = "이메일이 중복이 되는지 확인합니다.", tags = {"회원 가입"})
     public ResponseEntity<? extends BaseResponseDTO<String>> verifyEmail(@RequestBody AuthDTO verificationCode){
         BaseResponseDTO<String> response = memberService.verifyEmail(verificationCode);
         return ResponseEntity.status(response.getStatusCode()).body(response);
     }
 
     @PostMapping("/signin")
-    @Operation(summary = "로그인", description = "Id, PW로 로그인 진행", tags = {"View"})
+    @Operation(summary = "로그인", description = "Id, PW로 로그인 진행", tags = {"메인 페이지"})
     public ResponseEntity<? extends BaseResponseDTO<TokenDTO>> signIn(@RequestBody MemberLoginDTO memberInfo, HttpServletResponse httpResponse){
         BaseResponseDTO<TokenDTO> response = memberService.signIn(memberInfo, httpResponse);
         return ResponseEntity.status(response.getStatusCode()).body(response);
     }
 
     @PostMapping("/signout")
-    @Operation(summary = "로그 아웃", description = "버튼 클릭시 로그아웃 진행", tags = {"View"})
+    @Operation(summary = "로그 아웃", description = "버튼 클릭시 로그아웃 진행[구현 안됨]", tags = {"설정"})
     public ResponseEntity<?> signOut(HttpServletResponse httpResponse){
         return null;
     }
 
 
     @PostMapping("/inform")
-    @Operation(summary = "사용자 정보", description = "Post를 통해 받은 Email을 통해 정보를 받아옴", tags = {"View"})
+    @Operation(summary = "사용자 정보", description = "Post를 통해 받은 Email을 통해 정보를 받아옴", tags = {"설정"})
     public  ResponseEntity<? extends BaseResponseDTO<Member>> getInfo(@RequestBody MemberInformDTO memberInfo){
         BaseResponseDTO<Member> response = memberService.getInfo(memberInfo);
         return ResponseEntity.status(response.getStatusCode()).body(response);
